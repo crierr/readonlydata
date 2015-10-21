bdfc9c118d805662986da65ebc3d70944c8f6a51
Lee, SungUk <sulee@ea.com>
2015-10-21 19:40:17 +0900

    Support node version 4.x

--- test/base.js
+++ test/base.js
@@ -12,10 +12,13 @@ var fs = require('fs');
 var assert = require('assert');
 var readonlydata = require('../index');
 
+var JSON_FILE = 'test.json';
+var JSON_FILE_HASH = '29554fc6d1411659c9fcf84aa2d6113047973a1b'
+
 before(function () {
-	fs.writeFileSync('test.json', JSON.stringify(a));
+	fs.writeFileSync(JSON_FILE, JSON.stringify(a));
 	try {
-		fs.unlinkSync('test.json.cache');
+		fs.unlinkSync([JSON_FILE, JSON_FILE_HASH].join('.'));
 	} catch (e) {}
 });
 
@@ -35,7 +38,7 @@ describe('cache', function () {
 		assert.deepEqual(json, a);
 	});
 	it('cache should exist', function () {
-		assert(fs.existsSync('test.json.cache'));
+		assert(fs.existsSync([JSON_FILE, JSON_FILE_HASH].join('.')));
 	});
 	it('require again', function () {
 		var cached = readonlydata.require('test.json');
